3c1cb0603fd1d011681f21d4df40953b1e051f05
Developer Awdhesh <awdheshthinker@gmail.com>
2023-09-09 13:55:03 +0530

    basic setup

---
+++ src/Components/City.jsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  Badge,
+  Box,
+  Center,
+  Grid,
+  Heading,
+  Image,
+  Spinner,
+} from "@chakra-ui/react";
+import AddNew from "./AddNew";
+
+function City(props) {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getData = () => {
+    try {
+      axios.get(`http://localhost:3000/cities`).then((res) => {
+        console.log(res.data);
+        setData(res.data);
+        setLoading(false);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  if (loading) {
+    return (
+      <Center>
+        {" "}
+        <Spinner size={"md"} />
+      </Center>
+    );
+  }
+  return (
+    <>
+      <Center>
+        <Heading>Top 10 Cities in UP</Heading>
+        <AddNew />
+      </Center>
+      <Grid gridTemplateColumns={"repeat(3, 1fr)"} marginTop={"4rem"}>
+        {data.length > 0 &&
+          data.map((element, index) => {
+            return (
+              <Box
+                key={index}
+                maxW="sm"
+                borderWidth="1px"
+                borderRadius="lg"
+                overflow="hidden"
+              >
+                <Image src={element.img} alt="city image" />
+
+                <Box p="6">
+                  <Box display="flex" alignItems="baseline">
+                    <Badge borderRadius="full" px="2" colorScheme="teal">
+                      {element.name}
+                    </Badge>
+                  </Box>
+
+                  <Box
+                    mt="1"
+                    fontWeight="semibold"
+                    as="h4"
+                    lineHeight="tight"
+                    noOfLines={1}
+                  >
+                    {element.description}
+                  </Box>
+                </Box>
+              </Box>
+            );
+          })}
+      </Grid>
+    </>
+  );
+}
+
+export default City;

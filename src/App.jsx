b397ea8bf5171358a18503659c32a8b4d25554ff
Developer Awdhesh <awdheshthinker@gmail.com>
2023-09-09 13:55:23 +0530

    successfully display on UI

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Heading } from "@chakra-ui/react";
+import React from "react";
+import City from "./Components/City";
+function App(props) {
+  return (
+    <>
+
+      <City />
+    </>
+  );
+}
+
+export default App;

34d2063f86380ed320006799d00ede06adfdd9fb
Developer Awdhesh <awdheshthinker@gmail.com>
2023-09-09 14:04:08 +0530

    completed

--- src/Components/AddNew.jsx
+++ src/Components/AddNew.jsx
@@ -1,67 +1,86 @@
 import {
-  Button,
-  FormControl,
-  FormLabel,
-  Input,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
-  useDisclosure,
-} from "@chakra-ui/react";
-import React from "react";
+    Button,
+    FormControl,
+    FormLabel,
+    Input,
+    Modal,
+    ModalBody,
+    ModalCloseButton,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    ModalOverlay,
+    useDisclosure,
+  } from "@chakra-ui/react";
+  import React, { useState } from "react";
+  
+  function AddNew(props) {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+  
+    const initialRef = React.useRef(null);
+    const finalRef = React.useRef(null);
+    
+    const [formData, setFormData] = useState({
+        name:"",
+        description:"",
+        img:""
+    }) 
 
-function AddNew(props) {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+    const handleChange =(e)=>{
+        const {name, value}  = e.target
 
-  const initialRef = React.useRef(null);
-  const finalRef = React.useRef(null);
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
-  return (
-    <>
-      <Button onClick={onOpen}>Add New City</Button>
-
-      <Modal
-        initialFocusRef={initialRef}
-        finalFocusRef={finalRef}
-        isOpen={isOpen}
-        onClose={onClose}
-      >
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>New City</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody pb={6}>
-            <form onSubmit={handleSubmit}>
-              <FormControl>
-                <FormLabel>City name</FormLabel>
-                <Input ref={initialRef} placeholder="City name" required/>
-              </FormControl>
-
-              <FormControl mt={4}>
-                <FormLabel>Description</FormLabel>
-                <Input placeholder="Description" required />
-                <FormLabel>Image</FormLabel>
-                <Input placeholder="Image" required />
-              </FormControl>
-            </form>
-          </ModalBody>
-
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} type="submit">
-              Save
-            </Button>
-            <Button onClick={onClose}>Cancel</Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    </>
-  );
-}
-
-export default AddNew;
+        setFormData({...formData, [name]: value})
+    }
+    const handleSubmit = (e) => {
+      e.preventDefault();
+       console.log(formData);
+       console.log("click");
+    };
+  
+    return (
+      <>
+        <Button onClick={onOpen}>Add New City</Button>
+  
+        <Modal
+          initialFocusRef={initialRef}
+          finalFocusRef={finalRef}
+          isOpen={isOpen}
+          onClose={onClose}
+        >
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>New City</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody pb={6}>
+              <form onSubmit={handleSubmit}>
+                <FormControl>
+                  <FormLabel>City name</FormLabel>
+                  <Input name="city"  placeholder="City name" onChange={handleChange} required />
+                </FormControl>
+  
+                <FormControl mt={4}>
+                  <FormLabel>Description</FormLabel>
+                  <Input name="description"  placeholder="Description" onChange={handleChange} required />
+                </FormControl>
+  
+                <FormControl mt={4}>
+                  <FormLabel>Image</FormLabel>
+                  <Input name="img" placeholder="Image URL" onChange={handleChange} required />
+                </FormControl>
+              </form>
+            </ModalBody>
+  
+            <ModalFooter>
+              <Button colorScheme="blue" mr={3} type="submit">
+                Save
+              </Button>
+              <Button onClick={onClose}>Cancel</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </>
+    );
+  }
+  
+  export default AddNew;
+  
